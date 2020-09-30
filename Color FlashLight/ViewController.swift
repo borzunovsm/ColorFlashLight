e7e4f91fdc704d18c88caddc30c06c4243dc5a43
Serega <borzunov_sm2mail.ru>
2020-09-30 17:08:47 +0300

    ColorFlashlight

--- Color FlashLight/ViewController.swift
+++ Color FlashLight/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   var  whatIsColor = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
     }
 
+    @IBAction func buttonPressed() {
+        whatIsColor += 1
+        print(whatIsColor )
+        switch whatIsColor {
+        case 1:
+            view.backgroundColor = .red
+        case 2:
+            view.backgroundColor = .orange
+        case 3:
+            view.backgroundColor = .yellow
+        case 4:
+            view.backgroundColor = .green
+        case 5:
+            view.backgroundColor = .cyan
+        case 6:
+            view.backgroundColor = .blue
+        case 7:
+            view.backgroundColor = .purple
+        default:
+            view.backgroundColor = .black
+            
+        }
+        
+        }
+    }
+    
 
-}
 
